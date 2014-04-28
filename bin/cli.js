60ffe12f80a50d86d80e8fb8dfea1665fad2087c
Peter van der Zee <peter+zee@surfly.com>
2014-04-28 10:15:24 +0200

    Desc updates

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ require('../data/parsers.js');
 require('../data/sources.js');
 
 Gonzales.gatherTests(Gonzales.nodeBench, {order:'parser'});
+console.log('Fetching resources:');
 Gonzales.nodeLoadSources();
 Gonzales.nodeLoadParsers();
+console.log('\nDraining queue');
 Gonzales.nodeDrain();
